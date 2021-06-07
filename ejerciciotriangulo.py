df2ba84567a2930dfbd89ed85f33765035170a1c
janethM <aleja.malave@outlook.com>
2021-06-07 16:23:00 -0500

    mi primer commit

---
+++ ejerciciotriangulo.py
@@ -0,0 +1,6 @@
+#calcular la base y la altura de un triangulo#
+
+base = float(input("ingresar la base del triangulo: "))
+altura = float(input("ingresar la altura del triangulo: "))
+
+print(f"El area del triangulo es {(base * altura)/2}")
