885f865d612d8dae86072c013ec80bd1b8f010e2
janethM <aleja.malave@outlook.com>
2021-06-06 21:26:30 -0500

    equis D

---
+++ ejercicio002.py/areayperimetro.py
@@ -0,0 +1,11 @@
+### calcular el area y perimetro de un cuadrado ###
+lado = float(input ("ingrese medida de lado del cuadrado"))
+
+#calculos
+perimetro = lado * 4
+area = lado * lado 
+
+#ejecucion
+print("el perimetro del cuadrado es : ", perimetro)
+print("el area del cuadrado es : ", area)
+ #esto es un comentario#
